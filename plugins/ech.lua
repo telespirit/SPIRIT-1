8a5ebf2085927bdfe815dc9a999836c67ff4c151
teleantispirit <sasantebyani26@gmail.com>
2016-04-16 17:48:37 +0430

    Create ech.lua

---
+++ plugins/ech.lua
@@ -0,0 +1,25 @@
+local function run(msg, matches)
+  local text = matches[1]
+  local b = 1
+
+  while b ~= 0 do
+    text = text:trim()
+    text,b = text:gsub('^!+','')
+  end
+  return text
+end
+
+return {
+  description = "Echo A Massage",
+  usage = {
+  "بگو [whatever]: Echo Massage",
+  "/echo [whatever]: Echo Massage ",
+  "echo [whatever]: Echo Massage ",
+  },
+  patterns = {
+    "^بگو +(.+)$",
+	"^[/!#]echo +(.+)$",
+	"^echo +(.+)$"
+  }, 
+  run = run 
+}
